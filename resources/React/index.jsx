5b11029670c69f9ff087bfb1c19a79c14ee2c803
vinybergamo <vinybergamo@gmail.com>
2022-09-10 12:28:13 -0300

    Fix: Props

--- resources/React/index.jsx
+++ resources/React/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/App.js'
+import App from './routes/Route.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
